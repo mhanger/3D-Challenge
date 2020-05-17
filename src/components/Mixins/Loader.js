e0c47064e6986248ebf621b3d583321230adec90
Marc Hanger <mhangeril@gmail.com>
2020-05-17 18:49:24 -0500

    Moved GLTFLoader to its own npm to fix testing issues with node packages, update test scripts

--- src/components/Mixins/Loader.js
+++ src/components/Mixins/Loader.js
@@ -1,4 +1,4 @@
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import GLTFLoader from 'three-gltf-loader';
 
 export const gltfLoader = {
   methods: {
--- tests/e2e/specs/home.e2e.js
+++ tests/e2e/specs/home.e2e.js
@@ -1,7 +1,15 @@
 describe('Home Page', () => {
   it('has the correct title and heading', () => {
     cy.visit('/')
-    cy.title().should('equal', 'Home | Vue Enterprise Boilerplate')
-    cy.contains('h1', 'Home Page')
+    cy.title().should('equal', '3D Challenge | Vue and Three')
+    cy.contains('h1', '3D Challenge')
   })
+
+  it("scene exists", () => {
+    cy.get('.scene').should('exist');
+  });
+  it("swatches container exists", () => {
+    cy.get('.swatches-container').should('exist');
+  });
+
 })
